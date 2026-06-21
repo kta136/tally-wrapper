0c6e7d6adb7d16ee7506dd76d2038bd52c74ef18
Kartikey <kk136@outlook.com>
2026-06-21 18:47:32 +0530

    Persist print setting selections from preview dialog

--- src/ShowroomBilling.Desktop/Views/Printing/PrintPreviewDialog.xaml.cs
+++ src/ShowroomBilling.Desktop/Views/Printing/PrintPreviewDialog.xaml.cs
@@ -2,7 +2,9 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using ShowroomBilling.Desktop.Services;
 using ShowroomBilling.Desktop.ViewModels;
+using ShowroomBilling.Desktop.ViewModels.Printing;
 
 namespace ShowroomBilling.Desktop.Views.Printing;
 
@@ -30,5 +32,26 @@ public partial class PrintPreviewDialog : UserControl
 
         preview.DialogWidth = Math.Clamp(preview.DialogWidth + e.HorizontalChange, 760, Math.Max(760, ActualWidth - 24));
         preview.DialogHeight = Math.Clamp(preview.DialogHeight + e.VerticalChange, 520, Math.Max(520, ActualHeight - 24));
+    }
+
+    private void OnPrintSettingSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is not ComboBox combo || combo.DataContext is not PrintPreviewViewModel preview)
+        {
+            return;
+        }
+
+        switch (combo.Tag as string)
+        {
+            case "Duplex" when combo.SelectedValue is PrintDuplexMode duplex:
+                preview.SelectedDuplexMode = duplex;
+                break;
+            case "Color" when combo.SelectedValue is PrintColorMode color:
+                preview.SelectedColorMode = color;
+                break;
+            case "Collation" when combo.SelectedValue is PrintCollationMode collation:
+                preview.SelectedCollationMode = collation;
+                break;
+        }
     }
 }
